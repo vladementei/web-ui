669cf69cc684bd0eac1988bf7ecdc73cf513f187
vladementei@gmail.com <Pravda1650>
2020-12-06 16:27:25 +0300

    feat: add embedded option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RxUnsubscribe} from './core/services/rx-unsubscribe';
 import {RestService} from './core/services/rest-service.service';
+import {merge, Observable, of} from 'rxjs';
+import {ActivationStart, Router} from '@angular/router';
+import {filter, map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent extends RxUnsubscribe {
+export class AppComponent extends RxUnsubscribe implements OnInit {
 
-  constructor(private restService: RestService) {
+  isEmbedded$: Observable<boolean>;
+
+  constructor(private restService: RestService,
+              private router: Router) {
     super();
     this.restService.updateServerUrls();
+  }
+
+  ngOnInit(): void {
+    this.isEmbedded$ = merge(of(true), this.router.events.pipe(
+      filter(event => event instanceof ActivationStart),
+      map((event: ActivationStart) => event.snapshot.queryParams.embedded === 'true')
+    ));
   }
 }
