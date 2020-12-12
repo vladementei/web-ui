c144842667af6e6e7fe3043e4a57109d7694346a
vladementei@gmail.com <Pravda1650>
2020-12-12 17:24:53 +0300

    refactor: midi converter

--- src/app/functions/midi-to-notes.function.ts
+++ src/app/functions/midi-to-notes.function.ts
@@ -2,16 +2,16 @@ import * as jsonMidi from '../../assets/data/midi-dictionary.json';
 
 
 export function midiToNotes(midiArray): string {
-  let notesTxt = '';
-  const tactThreshold = 16;
-  const lineThreshold = 32;
-  let lastTact = 0;
-  let lastLine = 0;
-  let sumDlit = 0;
+  let notesTxt: string = '';
+  const tactThreshold: number = 16;
+  const lineThreshold: number = 32;
+  let lastTact: number = 0;
+  let lastLine: number = 0;
+  let sumDlit: number = 0;
   for (let i = 1; i < midiArray.track[1].event.length - 1; i++) {
     if (midiArray.track[1].event[i].deltaTime != 0) {
-      const dlit = jsonMidi['Duration'][midiArray.track[1].event[i].deltaTime]['display'];
-      notesTxt += jsonMidi['Notes'][midiArray.track[1].event[i].data.slice(0, 1)]['display'] + dlit + ' ';
+      const dlit: string = jsonMidi.duration[midiArray.track[1].event[i].deltaTime].display;
+      notesTxt += jsonMidi.note[midiArray.track[1].event[i].data.slice(0, 1)].display + dlit + ' ';
       sumDlit += Number.parseInt(dlit);
       if (sumDlit - lastTact >= tactThreshold) {
         notesTxt += '|';
