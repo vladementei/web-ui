ab866fe65c4b27ef23b86141fcfeda9a7cbb9c42
vladementei@gmail.com <Pravda1650>
2021-02-28 12:25:56 +0300

    feat: fetch web-app from ui-server

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require("express");
-const bodyParser = require ("body-parser");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 
-const port = process.env.PORT || 3000;
 const app = express();
-app.use(bodyParser.json())
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Welcome to web ui"));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested With, Content-Type, Accept');
+  next();
+});
+
+
+app.use((req, res, next) => {
+  if (path.extname(req.path).length > 0) {
+    next();
+  } else {
+    res.sendFile(path.resolve(`${__dirname}/dist/web-ui/index.html`));
+  }
+});
+
+app.use('/', express.static(`${__dirname}/dist`));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.listen(port, () => {
   console.log(`⚡️[server]: UI server is running at port:${port}`);
