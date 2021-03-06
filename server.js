4a086ea9763bd7442699263cd2c1dedd76f4be9f
vladementei@gmail.com <Pravda1650>
2021-03-06 11:50:27 +0300

    feat: login page if token invalid

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const jwt = require("jsonwebtoken");
 const http = require("http");
+cookieParser = require('cookie-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,20 +16,41 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(/.*(.js|.html|web-ui|web-ui\/|\/)$/, (req, res, next) => {
   console.log(req.originalUrl);
-  const token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiQW5vbnltb3VzIiwiaWF0IjoxNjE1MDE3ODY5LCJleHAiOjE2MTUxMDQyNjksImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3QiLCJpc3MiOiJPdHZpbnRhIGNvcnAiLCJzdWIiOiJvdHZpbnRhQGdtYWlsLmNvbSJ9.Q2WibnXLN9EzvqC8tITkU1iUK3dua1CsXSyrIbSuqFLQPfP8vyyTCkF3e5u_xJ2DWfM-BTUlOxg3N1GLNN5YqP7G8GOEWaVBi405rZrXj-52oHfOeDB4xW6SaEU67YFlr18oKY9zWlhfKHrN9IVW_NF3ccllgKZUfSRh0Lat2kS6BW4i7FY6COiysm02nLYivZRccKTRdDMjqK66FGsKSxnXWjeehjoroSyDz3uo5_0c4LCJrrJ6RCLUwk14E1g7gUIWcGcGfDLE6zFNboQJQ4OYiy5iVI4VZ4BFdyLJedCM4DCreXNSFG-VqY7INipJlTkRETI3RvW1qQathKdT_A";
+  const token = req.cookies.token;
   //const token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiQW5vbnltb3VzIiwiaWF0IjoxNjE0NTA2OTA0LCJleHAiOjE2MTQ1OTMzMDQsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3QiLCJpc3MiOiJPdHZpbnRhIGNvcnAiLCJzdWIiOiJvdHZpbnRhQGdtYWlsLmNvbSJ9.FF_XyxHSBSJX2vy33e5XH84Dv6AsQlTEQNacxuz1uOSUyJZHq0ZWiCJJdH1Ebs_XDtLsrz9uzBwoCTuKx2wxu5JSa04Eqvt9T50tGuPD_KYqIhg1LpGEhZWRsFFKjtlvw7EbOUXIkBKao8cPuoIfUSgbZajnCYtEshmyzi9nUBiv-rjCpVPaofiKKWkUWfh-uKH-BhmP_D9NYf4GYwFwR6Bv2ZSQj4AZS_qEiNNKvpVL6hzj7X0k86fHrTaaSumzKhrYRg6-webvdosLzmmGA5QEGoIuw3Q7UNmHYIkXzBVvGgMqOKuqPr6y7tCTRg4QhVXmYBpHr1LSlYh04RhQEA";
   try {
     const legit = jwt.verify(token, PUBLIC_KEY);
     console.log("\nJWT verification result: ", legit);
   } catch (e) {
-    if (e.name === "TokenExpiredError") {
-      res.sendFile(path.resolve(`${__dirname}/dist/login/login.html`));
+    if (e.name === "TokenExpiredError" || e.name === "JsonWebTokenError") {
+      res.cookie('token', undefined).sendFile(path.resolve(`${__dirname}/dist/login/login.html`));
       return;
     }
   }
   next();
+});
+
+app.use('/login', (req, response) => {
+  const options = {
+    hostname: 'localhost',
+    port: 8085,
+    path: '/authorization/token',
+    method: 'GET'
+  }
+
+  http.request(options, res => {
+    res.on('data', token => {
+      response.cookie('token', token.toString()).redirect('/');
+    })
+  }).on('error', () => {
+    response.cookie('token', undefined).redirect('/');
+  }).end();
 });
 
 app.use((req, res, next) => {
@@ -41,8 +63,6 @@ app.use((req, res, next) => {
 
 app.use('/', express.static(`${__dirname}/dist`));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 
 const options = {
   hostname: 'localhost',
