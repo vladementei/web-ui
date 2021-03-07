af7a3e295029a2df7e7baf879886c9158e8ecafd
vladementei@gmail.com <Pravda1650>
2021-03-07 13:52:45 +0300

    feat: show error if login failed

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const jwt = require("jsonwebtoken");
 const http = require("http");
+const ejs = require("ejs");
 cookieParser = require('cookie-parser');
 
 const app = express();
@@ -17,14 +18,15 @@ app.use((req, res, next) => {
 });
 
 app.use(cookieParser());
+app.engine('html', ejs.renderFile);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/logout', (req, res) => {
-  res.cookie('token', undefined).status(200).sendFile(path.resolve(`${__dirname}/dist/login/login.html`));
+  res.cookie('token', undefined).status(200).render(path.resolve(`${__dirname}/dist/login/login.html`), {error: null});
 });
 
-app.use(/.*(.js|.html|web-ui|web-ui\/|\/)$/, (req, res, next) => {
+app.use(/.*(.js|.html|web-ui|web-ui\/|\/|web-ui\/animation|web-ui\/animation\/|web-ui\/uploading|web-ui\/uploading\/)$/, (req, res, next) => {
   console.log(req.originalUrl);
   const token = req.cookies.token;
   //const token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiQW5vbnltb3VzIiwiaWF0IjoxNjE0NTA2OTA0LCJleHAiOjE2MTQ1OTMzMDQsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3QiLCJpc3MiOiJPdHZpbnRhIGNvcnAiLCJzdWIiOiJvdHZpbnRhQGdtYWlsLmNvbSJ9.FF_XyxHSBSJX2vy33e5XH84Dv6AsQlTEQNacxuz1uOSUyJZHq0ZWiCJJdH1Ebs_XDtLsrz9uzBwoCTuKx2wxu5JSa04Eqvt9T50tGuPD_KYqIhg1LpGEhZWRsFFKjtlvw7EbOUXIkBKao8cPuoIfUSgbZajnCYtEshmyzi9nUBiv-rjCpVPaofiKKWkUWfh-uKH-BhmP_D9NYf4GYwFwR6Bv2ZSQj4AZS_qEiNNKvpVL6hzj7X0k86fHrTaaSumzKhrYRg6-webvdosLzmmGA5QEGoIuw3Q7UNmHYIkXzBVvGgMqOKuqPr6y7tCTRg4QhVXmYBpHr1LSlYh04RhQEA";
@@ -33,7 +35,11 @@ app.use(/.*(.js|.html|web-ui|web-ui\/|\/)$/, (req, res, next) => {
     console.log("\nJWT verification result: ", legit);
   } catch (e) {
     if (e.name === "TokenExpiredError" || e.name === "JsonWebTokenError") {
-      res.cookie('token', undefined).status(401).sendFile(path.resolve(`${__dirname}/dist/login/login.html`));
+      let params = {error: null};
+      if (req.param("authError") === "true") {
+        params.error = "Something went wrong!";
+      }
+      res.cookie('token', undefined).status(401).render(path.resolve(`${__dirname}/dist/login/login.html`), params);
       return;
     }
   }
@@ -46,11 +52,17 @@ app.use('/login', (req, response) => {
     port: 8085,
     path: '/authorization/token',
     method: 'GET'
+  };
+
+  const redirectUrl = (req.headers && req.headers.referer && req.headers.referer.slice(req.headers.referer.indexOf('/web-ui'))).replace('?authError=true', '') || '/';
+
+  if (!req.body.password) {
+    response.cookie('token', undefined).status(401).redirect(redirectUrl + '?authError=true');
+    return;
   }
 
   http.request(options, res => {
     res.on('data', token => {
-      const redirectUrl = (req.headers && req.headers.referer && req.headers.referer.slice(req.headers.referer.indexOf('/web-ui'))) || '/'
       response.cookie('token', token.toString()).redirect(redirectUrl);
     })
   }).on('error', () => {
